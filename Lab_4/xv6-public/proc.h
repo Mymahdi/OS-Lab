9c19f7385ee001cc4d442583cb0c0cab4466b9c3
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2024-12-28 15:14:28 +0330

    Add a new variable SysCallCounter

--- Lab_4/xv6-public/proc.h
+++ Lab_4/xv6-public/proc.h
@@ -8,6 +8,7 @@ struct cpu {
   int ncli;                    // Depth of pushcli nesting.
   int intena;                  // Were interrupts enabled before pushcli?
   struct proc *proc;           // The process running on this cpu or null
+  uint SysCallCounter; 
 };
 
 extern struct cpu cpus[NCPU];
