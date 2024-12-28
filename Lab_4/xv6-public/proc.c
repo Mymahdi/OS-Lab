a568684ec76828c8285378e8a0cd7252bbddaeea
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2024-12-28 15:19:46 +0330

    Initialize per-core SysCallCounter in mycpu() function

--- Lab_4/xv6-public/proc.c
+++ Lab_4/xv6-public/proc.c
@@ -34,23 +34,30 @@ cpuid() {
 
 // Must be called with interrupts disabled to avoid the caller being
 // rescheduled between reading lapicid and running through the loop.
-struct cpu*
-mycpu(void)
+struct cpu* mycpu(void)
 {
   int apicid, i;
   
-  if(readeflags()&FL_IF)
+  if(readeflags() & FL_IF)
     panic("mycpu called with interrupts enabled\n");
-  
+
   apicid = lapicid();
+  
   // APIC IDs are not guaranteed to be contiguous. Maybe we should have
   // a reverse map, or reserve a register to store &cpus[i].
   for (i = 0; i < ncpu; ++i) {
-    if (cpus[i].apicid == apicid)
+    if (cpus[i].apicid == apicid) {
+      // Initialize SysCallCounter for the current CPU (if not already done)
+      if (cpus[i].SysCallCounter == 0) {
+        cpus[i].SysCallCounter = 0;  // Initialize SysCallCounter to 0
+      }
       return &cpus[i];
+    }
   }
+
   panic("unknown apicid\n");
 }
+
 
 // Disable interrupts so that we are not rescheduled
 // while reading proc from the cpu structure
