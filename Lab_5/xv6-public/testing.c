7c2e916e883f0528ce899fb9a85f154ea006085b
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2025-01-13 03:18:51 +0330

    Lab_5: Fix makefile

--- Lab_5/xv6-public/testing.c
+++ Lab_5/xv6-public/testing.c
@@ -3,7 +3,7 @@
 #include "user.h"
 #include "spinlock.h"
 
-#define PAGE 82
+#define NPAGE 82
 
 struct fac {
     int value;
@@ -12,7 +12,7 @@ struct fac {
 };
 
 void factorial(int count) {
-    struct fac* adr = (struct fac*)openshmem(PAGE);
+    struct fac* adr = (struct fac*)openshmem(NPAGE);
     initspin(&adr[0].lock);
     accspin(&adr[0].lock);
     adr[0].value = 1;
@@ -22,7 +22,7 @@ void factorial(int count) {
 
     for (int i = 1; i < count; i++) {
         if (fork() == 0) {
-            struct fac* adrs = (struct fac*)openshmem(PAGE);
+            struct fac* adrs = (struct fac*)openshmem(NPAGE);
             for (int j = 0; j < 100000; j++)
                 ;
             accspin(&adr[0].lock);
@@ -33,13 +33,13 @@ void factorial(int count) {
             printf(1, "%d\n", adrs[0].value);
 
             relspin(&adr[0].lock);
-            closeshmem(PAGE);
+            closeshmem(NPAGE);
             exit();
         }
     }
     for (int i = 0; i < count; i++)
         wait();
-    closeshmem(PAGE);
+    closeshmem(NPAGE);
 }
 
 int main(int argc, char* argv[]) {
